edc2362e1b63e6fdaadd99904b480bf51ffd9242
thomas.li <4167023@qq.com>
2017-06-02 08:49:46 +0800

    fix duplicated logger issue.

--- webconfig/piplib/sharelib/educloud-luhyaapi/luhyaapi/educloudLog.py
+++ webconfig/piplib/sharelib/educloud-luhyaapi/luhyaapi/educloudLog.py
@@ -13,14 +13,14 @@ def set_log_level(level, loggername='luhya'):
     elif level == 'info':
         logger.setLevel(logging.INFO)
 
-
 def init_log(logfile, loggername='luhya'):
     logger = logging.getLogger(loggername)
-    ch = logging.handlers.RotatingFileHandler(logfile, maxBytes=MAX_LOGFILE_BYTE, backupCount=MAX_LOG_COUNT)
-    formatter = logging.Formatter('<%(asctime)s> <%(levelname)s> <%(module)s:%(lineno)d>\t%(message)s', datefmt='%F %T')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    logger.setLevel(logging.ERROR)
+    if len(logger.handlers) == 0:
+        ch = logging.handlers.RotatingFileHandler(logfile, maxBytes=MAX_LOGFILE_BYTE, backupCount=MAX_LOG_COUNT)
+        formatter = logging.Formatter('<%(asctime)s> <%(levelname)s> <%(module)s:%(lineno)d>\t%(message)s', datefmt='%F %T')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        logger.setLevel(logging.ERROR)
     return logger
 
 WEB_LOG_FILE        = '/var/log/educloud/educloud.log'
